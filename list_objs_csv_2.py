4fc3a92d64744c1e57460a7f9d80fb9e660416d7
Suu J <sudhanshu.kmsh@gmail.com>
2025-11-19 15:50:13 +0530

    list objects with a specified string as key

---
+++ list_objs_csv_2.py
@@ -0,0 +1,40 @@
+import boto3
+import csv
+
+#we are just making sure that we only load objs with the name conversation id
+aws_access_key_id = ''
+aws_secret_access_key = ''
+aws_region_name = ''
+s3_bucket_name = ''
+
+s3_client = boto3.client(
+    's3',
+    region_name=aws_region_name,
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
+
+
+def list_s3_objects(bucket_name):
+    paginator = s3_client.get_paginator('list_objects_v2')
+    for page in paginator.paginate(Bucket=bucket_name):
+        for content in page.get('Contents', []):
+            yield content['Key']
+
+
+def save_to_csv(object_keys, filename='s3_file_list2.csv'):
+    with open(filename, 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile)
+        filewriter.writerow(['Filename'])
+        for key in object_keys:
+            
+            if 'conversation_id=' in key:
+                filewriter.writerow([key])
+
+
+def main():
+    object_keys = list_s3_objects(s3_bucket_name)
+    save_to_csv(object_keys)
+
+if __name__ == "__main__":
+    main()
